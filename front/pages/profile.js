81986f9d13cf31ef1ebac47ad97039d58c4c9787
ppby <salswnss@gmail.com>
2020-07-18 17:10:53 +0900

    Setting 프로필 페이지 만들기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Head from 'next/Head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditFrom from '../components/NicknameEditFrom';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+  // 더미 데이터 만들기
+  const followingList = [
+    { nickname: '서민준' },
+    { nickname: '바보' },
+    { nickname: 'ppby' },
+  ];
+  const followerList = [
+    { nickname: '서민준' },
+    { nickname: '바보' },
+    { nickname: 'ppby' },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필 | ppby sparrow</title>
       </Head>
       <AppLayout>
-        <div>프로필 페이지</div>
+        {/* 처음 페이지 설계시 컴포넌트를 만들지 않아도 어떤 컴포넌트가 들어가는지 생각해서 미리 작성(프롭스도 미리 설정) */}
+        <NicknameEditFrom />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
