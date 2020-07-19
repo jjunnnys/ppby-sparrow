81dc6cc4b941da9ef18a79ad58b5ae121e5fb7a5
ppby <salswnss@gmail.com>
2020-07-19 16:25:12 +0900

    Add 이미지 구현하기

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,8 +1,81 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
 
+// 이미지가 한개 이상있을 때 PostImages 컴포넌트가 작성된다.
 const PostImages = ({ images }) => {
-  return <div>구현 중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  if (images.length === 1) {
+    return (
+      <>
+        {/* role="presentation" -> 스크린리더가 이걸 클릭은 할 수 있지만 꼭 클릭할 필요는 없다라고 알려 줌 (버튼이나 인풋이 아닌 태그에 onClick을 쓸 경우 작성) */}
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <div style={{ display: 'felx' }}>
+        <img
+          style={{
+            width: '50%',
+            display: 'inline-block',
+          }}
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          style={{
+            width: '50%',
+            display: 'inline-block',
+          }}
+          role="presentation"
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <img
+        style={{
+          width: '50%',
+          display: 'inline-block',
+        }}
+        role="presentation"
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+      <div
+        role="presentation"
+        style={{
+          display: 'inline-block',
+          width: '50%',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+        }}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1}
+        개의 사진 더보기
+      </div>
+    </>
+  );
 };
 
 PostImages.prototype = {
